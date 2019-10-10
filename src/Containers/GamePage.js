e3d0407b96c636074118ec7a07dbdc4c4e5e1531
1cb8c1 <6art10miejm0ty1@gmail.com>
2019-10-10 13:04:27 +0200

    made rows work

--- src/Containers/GamePage.js
+++ src/Containers/GamePage.js
@@ -6,10 +6,18 @@ export const GamePage = () => {
     <div className="parallax">
       <div className="parallax__layer parallax__layer--background"></div>
       <div className="parallax__layer parallax__layer--background2"></div>
-      <div className="parallax__layer parallax__layer--background3"></div>
-      <div className="parallax__layer parallax__layer--base">
-        <div className></div>
+      <div className="parallax__layer parallax__layer--layer1">
+        <div className="quoteFrame"></div>
+      </div>
+      <div className="parallax__layer parallax__layer--base flex-container">
+        <div className="quote">
+          And he shall rule them with a rod of iron, and as the vessels of a
+          potter, shall they be broken
+        </div>
       </div>
     </div>
   );
 };
+
+/*              And he shall rule them with a rod of iron, and as the vessels of a
+              potter, shall they be broken*/
