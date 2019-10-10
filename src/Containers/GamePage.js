a84661519b2a5310ae262344018e44efcd0b7a8b
1cb8c1 <6art10miejm0ty1@gmail.com>
2019-10-10 11:35:49 +0200

    more styling

--- src/Containers/GamePage.js
+++ src/Containers/GamePage.js
@@ -1,7 +1,18 @@
 import React from "react";
-import { Dummy } from "../texts/GamePage.json";
 import "../Styles/GameStyle.scss";
 
 export const GamePage = () => {
-  return <div className="GamePage"></div>;
+  return (
+    <div class="parallax">
+      <div class="parallax__layer parallax__layer--background">
+        <div class="title">This is the background</div>
+      </div>
+      <div class="parallax__layer parallax__layer--background2">
+        <div class="title">This is the background2</div>
+      </div>
+      <div class="parallax__layer parallax__layer--base">
+        <div class="title">This is the base</div>
+      </div>
+    </div>
+  );
 };
